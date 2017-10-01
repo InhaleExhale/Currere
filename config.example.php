2247607636b9dc76e90551504fe8eca6e5a6fabd
InhaleExhale <31828525+InhaleExhale@users.noreply.github.com>
2017-10-01 17:33:58 +0100

    Created example config file

---
+++ config.example.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Rename this file to config.php and change templated values to match your ones.
+ */
+
+class Config
+{
+    public static $config = array(
+        "core" => array(
+            "useSef" => false,
+            "appSecret" => "hello_goodbye_1024",
+            "appRoot" => "/Currere"
+        ),
+        "database" => array(
+            "name" => "<DB_NAME>",
+            "username" => "<USERNAME>",
+            "password" => "<PASSWORD>",
+
+        ),
+        "strava" => array(
+            "clientId" => "<STRAVA_CLIENT_ID>",
+            "clientSecret" => "<STRAVA_CLIENT_SECRET>"
+        ),
+        "runkeeper" => array(
+            "clientId" => "<RUNKEEPER_CLIENT>",
+            "clientSecret" => "<RUNKEEPER_CLIENT_SECRET>"
+        )
+    );
+
+    public static function get($path) {
+        $parts = explode("/", $path);
+        $obj = self::$config;
+
+        foreach ($parts as $part) {
+            if(!isset($obj[$part])) {
+                return null;
+            }
+            $obj = $obj[$part];
+        }
+        return $obj;
+
+
+    }
+}
